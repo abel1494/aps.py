32302abb239b49a0aecc4ef5b258ed1f0e5e0c43
abel1494 <118557596+abel1494@users.noreply.github.com>
2025-06-14 22:06:40 +0700

    Update aps.py

--- aps.py
+++ aps.py
@@ -106,7 +106,7 @@ st.markdown("""
 def load_model():
     """Load tokenizer dan model dengan caching untuk performa optimal"""
     model_name = "Alsyabel/AIdetect"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=false)
     model = AutoModelForSequenceClassification.from_pretrained(model_name)
     return tokenizer, model
 
